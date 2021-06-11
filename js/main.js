25b898715ad79c61492d07dba817d844dfd449e9
Marieta Xachatryan <69346003+Marieta28@users.noreply.github.com>
2021-06-12 01:53:34 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const menuButton = document.querySelector(".menu-button");
+const menu = document.querySelector(".nav-list");
+const menuCloseButton = document.querySelector(".menu-close");
+
+
+menuButton.addEventListener("click", () =>
+{
+menu.classList.add("is-active");
+menuCloseButton.classList.add("is-active");
+});
+
+menuCloseButton.addEventListener("click", () =>
+{
+  menu.classList.remove("is-active");
+menuCloseButton.classList.remove("is-active");
+});
